3a8c27d4a35c09adc2de2a17207d3abe2f8cd442
IsaacPH2005 <parisacahuallpaisaac@gmail.com>
2025-01-31 01:13:04 -0400

    Ciclo for en java xd

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        for(int x = 1; x<=8; x++){
+            System.err.println(x + " hola mundo");
+        }
+    }
+}
